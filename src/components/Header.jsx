b4ed445e6f39bc24d8e1c7702c79474c6706311f
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2026-01-26 19:52:57 +0100

    feat: migliorie

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function Header () {
+export default function Header() {
   return (
     <header>
       <nav className="navbar navbar-expand-lg bg-primary">
         <div className="container-fluid">
-          <Link className="navbar-brand" href="#">BooRoad</Link>
+          <Link className="navbar-brand" href="#">
+            BooRoad
+          </Link>
         </div>
       </nav>
     </header>
