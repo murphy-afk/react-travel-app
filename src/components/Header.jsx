819e3e6295493469047a7a1e845b7264b18a1618
murphy-afk <bardarenoemi@gmail.com>
2026-01-27 18:17:12 +0100

    feat: styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -7,7 +7,7 @@ export default function Header() {
         className="navbar navbar-expand-lg py-3 nav-blur"
       >
         <div className="container">
-          <Link className="navbar-brand d-flex align-items-center" to="/">
+          <NavLink className="navbar-brand d-flex align-items-center" to="/">
             <div
               className="rounded-3 p-1 me-2 shadow-lg diagonal-gradient"
             >
@@ -20,7 +20,7 @@ export default function Header() {
             >
               BOOROAD
             </span>
-          </Link>
+          </NavLink>
 
           <button
             className="navbar-toggler border-0 text-white invert"
@@ -34,28 +34,22 @@ export default function Header() {
           <div className="collapse navbar-collapse" id="navBooRoad">
             <ul className="navbar-nav ms-auto align-items-center">
               <li className="nav-item me-lg-4">
-                <Link
-                  className="nav-link fw-bold text-light opacity-75 hover-opacity-100 transition-all text-small-spaced"
+                <NavLink
+                  className="btn text-light mb-1 rounded-pill"
                   to="/"
                 >
                   ESPLORA
-                </Link>
+                </NavLink>
               </li>
 
               <li className="nav-item">
-                <Link
-                  className="btn px-4 py-2 fw-bold text-white border-0 shadow-glow rounded-pill d-flex align-items-center gradient-hover"
-                  onMouseOver={(e) =>
-                    (e.currentTarget.style.transform = "scale(1.05)")
-                  }
-                  onMouseOut={(e) =>
-                    (e.currentTarget.style.transform = "scale(1)")
-                  }
+                <NavLink
+                  className="btn text-light rounded-pill"
                   to="/addtrips"
                 >
                   <i className="bi bi-plus-lg me-2"></i>
-                  ORGANIZZA
-                </Link>
+                  NUOVI VIAGGI
+                </NavLink>
               </li>
             </ul>
           </div>
